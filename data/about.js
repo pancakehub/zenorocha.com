2dd9e8043f85633e9783e343f24a9b381ff0ea32
LOHITH REDDY DONURU <22ph102a08@anurag.edu.in>
2024-10-17 03:49:36 +0530

    Add files via upload

---
+++ data/about.js
@@ -0,0 +1,68 @@
+const items = [
+  {
+    jobTitle: 'Intern Analytics',
+    company: 'NRSC, ISRO',
+    companyUrl: 'https://resend.comhttps://www.nrsc.gov.in/aboutus_campus_nrscjdm',
+    startDate: '2024-01-22',
+    endDate: '2024-05-31',
+    location: 'Hyderabad, Telangana',
+  },
+  {
+    jobTitle: 'Coding Club Manager',
+    company: 'Vignan University',
+    companyUrl: 'https://vignanits.ac.in/',
+    startDate: '20223-05-14',
+    endDate: '2024-06-18',
+    location: 'Hyderabad, Telangana',
+  },
+/*  {
+    jobTitle: 'Chief Product Officer',
+    company: 'Liferay Cloud, Inc',
+    companyUrl: 'https://liferay.com',
+    startDate: '2018-06-01',
+    endDate: '2021-07-01',
+    location: 'Los Angeles, California',
+  },
+  {
+    jobTitle: 'Principal Developer Advocate',
+    company: 'Liferay, Inc',
+    companyUrl: 'https://liferay.com',
+    startDate: '2015-03-01',
+    endDate: '2018-06-01',
+    location: 'Los Angeles, California',
+  },
+  {
+    jobTitle: 'Front-end Engineer',
+    company: 'Liferay Latin America',
+    companyUrl: 'https://liferay.com',
+    startDate: '2012-07-01',
+    endDate: '2015-03-01',
+    location: 'Recife, Brazil',
+  },
+  {
+    jobTitle: 'Front-end Engineer',
+    company: 'Globo',
+    companyUrl: 'https://globo.com',
+    startDate: '2011-10-01',
+    endDate: '2012-07-01',
+    location: 'Rio de Janeiro, Brazil',
+  },
+  {
+    jobTitle: 'Software Engineer',
+    company: 'Petrobras',
+    companyUrl: 'https://petrobras.com.br',
+    startDate: '2011-02-01',
+    endDate: '2011-10-01',
+    location: 'Rio de Janeiro, Brazil',
+  },
+  {
+    jobTitle: 'Web Developer',
+    company: 'Caos! Design',
+    companyUrl: 'http://caosdesign.com.br',
+    startDate: '2009-09-01',
+    endDate: '2011-02-01',
+    location: 'Rio de Janeiro, Brazil',
+  },*/
+]
+
+export default items
